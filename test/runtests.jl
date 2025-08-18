5295fa3e4d793957398961aaeda510644d44ef69
Constantin Jung <conijung@gmx.de>
2025-08-18 14:46:41 +0200

    completed julia files

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "Fibonacci.jl" begin
     # Write your tests here.
+    @test fibonacci(0) == (BigInt(0), BigInt(1))
+    @test fibonacci(1) == (BigInt(1), BigInt(1))
+    @test fibonacci(30) == (BigInt(832040), BigInt(1346269))
 end
