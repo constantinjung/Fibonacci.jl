e02767d7f412ecb1e5cd7a1799bfdf8a403ae19e
Documenter.jl <documenter@juliadocs.github.io>
2025-08-25 12:32:02 +0000

    build based on d716d72

--- dev/literate/the_fibonacci_example_script.jl
+++ dev/literate/the_fibonacci_example_script.jl
@@ -1,6 +1,36 @@
-# # Fibonacci numbers
+# # Fibonacci function tutorial
 #
-# Compute the n-th and (n+1)-th Fibonacci numbers using the fast doubling method.
+# In this tutorial we demonstarte how to use the `fibonacci` function
+# form the `Fibonacci.jl` package.
+#
+# we start by loading the package:
 
 using Fibonacci
+
+# ## First steps
+#
+# Let's compute the 10th Fibonacci number:
+
 fibonacci(10)
+
+# The result should be `55`.
+#
+# You can also compute larger values easily:
+
+fibonacci(20)
+
+# which gives `6765`.
+#
+# For very large `n`, Julia automatically uses `BigInt` to hold the result:
+
+fibonacci(BigInt(100))
+
+# ## Summary
+#
+# The `fibonacci` function is simple to use:
+#
+# - Call it with an integer argument `n`.
+# - It returns the `n`th Fibonacci number.
+# - It works for both `Int` and `BigInt`.
+#
+# This makes it suitable for teaching, experiments, or mathematical explorations.
